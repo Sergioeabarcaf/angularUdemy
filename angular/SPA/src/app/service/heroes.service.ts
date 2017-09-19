dd6137164f6786493b8d32a9e0b16568f176e00d
sergioeabarcaf <sergioaf1991@gmail.com>
2017-09-19 13:12:36 -0300

    Se agregan la interfaz de heroes

--- angular/SPA/src/app/service/heroes.service.ts
+++ angular/SPA/src/app/service/heroes.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class HeroesService {
 
-  private heroes:any[] = [
+  private heroes:Heroe[] = [
     {
       nombre: "Aquaman",
       bio: "El poder más reconocido de Aquaman es la capacidad telepática para comunicarse con la vida marina, la cual puede convocar a grandes distancias.",
@@ -59,7 +59,15 @@ export class HeroesService {
   constructor() {
   console.log("servicio listo") }
 
-  getHeroes(){
+  getHeroes():Heroe[]{
     return this.heroes;
   }
+}
+
+export interface Heroe{
+  nombre:string;
+  bio:string;
+  img:string;
+  aparicion:string;
+  casa:string;
 }
