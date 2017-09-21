05cec6dfdd89938760f2839dd4c8cbcfce54eb9c
sergioeabarcaf <sergioaf1991@gmail.com>
2017-09-21 09:06:03 -0300

    se crean las funciones del heroe-search

--- angular/SPA/src/app/components/heroe-search/heroe-search.component.ts
+++ angular/SPA/src/app/components/heroe-search/heroe-search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../service/heroes.service';
 
 @Component({
   selector: 'app-heroe-search',
@@ -6,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeSearchComponent implements OnInit {
 
-  constructor() { }
+  heroeBuscar:any[] = [];
+  termino:string;
+
+  constructor( private activatedRoute:ActivatedRoute, private _heroesService:HeroesService ) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe( params =>{
+      this.termino = params['termino'];
+      this.heroeBuscar = this._heroesService.buscarHeroe( params['termino']);
+      console.log(this.heroeBuscar);
+    })
   }
 
 }
