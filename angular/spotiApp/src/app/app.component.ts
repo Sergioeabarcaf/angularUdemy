cd2c3d6d4d782616eda68b4509e2377aed3f55a3
sergioeabarcaf <sergioaf1991@gmail.com>
2017-10-11 22:35:56 -0300

    Se crea la carpeta para proyecto SpotiApp

--- angular/spotiApp/src/app/app.component.ts
+++ angular/spotiApp/src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre = "Sergio";
-  activar:boolean = true;
+  title = 'app';
 }
--- angular/spotiApp/src/app/app.module.ts
+++ angular/spotiApp/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ContrasenaPipe } from './pipe/contrasena.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContrasenaPipe
+    AppComponent
   ],
   imports: [
     BrowserModule
