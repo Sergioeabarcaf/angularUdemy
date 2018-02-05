2f775291f86d7e82a8ac7aecfd40c317f2a7535d
sergioeabarcaf <sergioaf1991@gmail.com>
2018-02-05 08:48:28 -0300

    se agrega un flag de funcionamiento

--- angular/spotiApp/src/app/services/spoti-app.service.ts
+++ angular/spotiApp/src/app/services/spoti-app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SpotiAppService {
 
-  constructor() { }
+  constructor() {
+    console.log("Servicio SpotiApp listo!");
+  }
 
 }
