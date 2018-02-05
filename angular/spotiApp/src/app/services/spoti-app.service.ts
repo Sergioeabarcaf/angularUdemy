4b7001a0ebfd91e59880510a58b6a5d6c49cf51e
sergioeabarcaf <sergioaf1991@gmail.com>
2018-02-05 09:03:26 -0300

    utilizacion de modulo httpClient para hacer get sin autorizacion

--- angular/spotiApp/src/app/services/spoti-app.service.ts
+++ angular/spotiApp/src/app/services/spoti-app.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class SpotiAppService {
 
-  constructor() {
+  constructor( public http: HttpClient ) {
     console.log("Servicio SpotiApp listo!");
+  }
+
+  getArtistas(){
+    let url = "https://api.spotify.com/v1/search?q=metallica&type=artist"
+
+    this.http.get(url).subscribe(res => {
+      console.log(res);
+    })
   }
 
 }
