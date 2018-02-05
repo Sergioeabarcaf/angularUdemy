3f69ae6f40ceba20023366216d35b08297d2e67f
sergioeabarcaf <sergioaf1991@gmail.com>
2018-02-04 21:41:21 -0300

    importacion del ServiceSpotify

--- angular/spotiApp/src/app/components/search/search.component.ts
+++ angular/spotiApp/src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../service/spotify.service';
 
 @Component({
   selector: 'app-search',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor( public _spotify:SpotifyService ) {
+    this._spotify.getArtista('romeo').subscribe(resp =>{
+      console.log("informacion lista");
+      console.log(resp);
+    });
+
+   }
+
 
   ngOnInit() {
   }
