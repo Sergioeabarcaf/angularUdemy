4f4a6828d0e82f070aa075cf0cb15df28f520a9e
sergioeabarcaf <sergioaf1991@gmail.com>
2018-02-06 18:57:55 -0300

    eliminacion de estilos propios en artist

--- angular/spotiApp/src/app/components/artist/artist.component.ts
+++ angular/spotiApp/src/app/components/artist/artist.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-artist',
   templateUrl: './artist.component.html',
-  styleUrls: ['./artist.component.css']
+  styles: []
 })
 export class ArtistComponent implements OnInit {
 
