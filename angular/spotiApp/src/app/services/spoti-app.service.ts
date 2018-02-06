5bb10b79328a494ff17a1e00444a0fe7fcb250e5
sergioeabarcaf <sergioaf1991@gmail.com>
2018-02-05 22:34:51 -0300

    se agrega modulo httpHeaders, un arreglo de artistas y tambien elimina el subsribe

--- angular/spotiApp/src/app/services/spoti-app.service.ts
+++ angular/spotiApp/src/app/services/spoti-app.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class SpotiAppService {
+
+  artistas:any[] = [];
 
   constructor( public http: HttpClient ) {
     console.log("Servicio SpotiApp listo!");
   }
 
   getArtistas(){
-    let url = "https://api.spotify.com/v1/search?q=metallica&type=artist"
+    let url = "https://api.spotify.com/v1/search?q=metallica&type=artist";
+    let headers = new HttpHeaders({
+      'authorization': 'Bearer BQCDiIy9_GXFXMB26rfMlDfSWbI26MW2DTmJmGM6cd88L6p8jRqPbYyU6qd4CpuujhHrEYc39k45tK6oa00'
+    });
 
-    this.http.get(url).subscribe(res => {
-      console.log(res);
-    })
+    return this.http.get(url, { headers })
   }
 
 }
