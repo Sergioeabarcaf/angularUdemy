bf72b5fa77daa123b0ac9e302fc779ff26c9c1e3
sergioeabarcaf <sergioaf1991@gmail.com>
2018-02-05 22:41:38 -0300

    importacion operador map

--- angular/spotiApp/src/app/services/spoti-app.service.ts
+++ angular/spotiApp/src/app/services/spoti-app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class SpotiAppService {
@@ -16,7 +17,9 @@ export class SpotiAppService {
       'authorization': 'Bearer BQCDiIy9_GXFXMB26rfMlDfSWbI26MW2DTmJmGM6cd88L6p8jRqPbYyU6qd4CpuujhHrEYc39k45tK6oa00'
     });
 
-    return this.http.get(url, { headers })
+    return this.http.get(url, { headers }).map( res =>{
+
+    })
   }
 
 }
