ee694518638daaa65f0313e25abc99e34d810779
sergioeabarcaf <sergioaf1991@gmail.com>
2018-02-27 18:11:30 -0300

    se usa la propiedad ElementRef para utilizar el elemento y cambiar las propiedades de el

--- angular/5 - miscelaneos/src/app/directives/resaltado.directive.ts
+++ angular/5 - miscelaneos/src/app/directives/resaltado.directive.ts
@@ -1,10 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appResaltado]'
 })
 export class ResaltadoDirective {
 
-  constructor() { }
+  constructor(private el:ElementRef) {
+    console.log("Directivas llamda")
+    el.nativeElement.style.backgroundColor = "yellow";
+  }
 
 }
