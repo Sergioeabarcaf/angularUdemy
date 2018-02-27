082931e3ba88d3fb9516769e883482c214a1f159
sergioeabarcaf <sergioaf1991@gmail.com>
2018-02-27 00:00:08 -0300

    uso de ngStyle con variable que son modificados con dos botones

--- angular/5 - miscelaneos/src/app/components/ng-style/ng-style.component.ts
+++ angular/5 - miscelaneos/src/app/components/ng-style/ng-style.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ng-style',
   template: `
-    <p>
-      ng-style Works!
+    <p [style.fontSize.px] = "tamano">
+      Hola mundo... esta es una etiqueta
     </p>
+
+    <button class="btn btn-primary" (click)="tamano = tamano + 5">
+      <i class="fa fa-plus"></i>
+    </button>
+
+    <button class="btn btn-primary" (click)="tamano = tamano - 5">
+      <i class="fa fa-minus"></i>
+    </button>
+
   `,
   styles: []
 })
 export class NgStyleComponent implements OnInit {
+
+  tamano:number = 10;
 
   constructor() { }
 
