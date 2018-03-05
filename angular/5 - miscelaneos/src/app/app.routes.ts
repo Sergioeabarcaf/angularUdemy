2af8c8028de4dc257eb3dd74f06aae0f0c30e442
sergioeabarcaf <sergioaf1991@gmail.com>
2018-03-05 12:35:15 -0300

    se agregan rutas hijas del componente usuario

--- angular/5 - miscelaneos/src/app/app.routes.ts
+++ angular/5 - miscelaneos/src/app/app.routes.ts
@@ -2,9 +2,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
 
+import { UsuarioDetalleComponent } from './components/usuarios/usuario-detalle.component';
+import { UsuarioEditarComponent } from './components/usuarios/usuario-editar.component';
+import { UsuarioNuevoComponent } from './components/usuarios/usuario-nuevo.component';
+
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'usuario', component: UsuariosComponent },
+  {
+    path: 'usuario/:10',
+    component: UsuariosComponent,
+    children:[
+      { path: 'nuevo', component: UsuarioNuevoComponent },
+      { path: 'editar', component: UsuarioEditarComponent },
+      { path: 'detalle', component: UsuarioDetalleComponent }
+    ]
+  },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
