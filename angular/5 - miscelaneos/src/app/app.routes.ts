84911fa6f4aa04e889048aeb21b5858c9d178bf6
sergioeabarcaf <sergioaf1991@gmail.com>
2018-03-05 12:21:06 -0300

    correccion de importacion usuarios

--- angular/5 - miscelaneos/src/app/app.routes.ts
+++ angular/5 - miscelaneos/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'usuario', component: UsuariosComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
