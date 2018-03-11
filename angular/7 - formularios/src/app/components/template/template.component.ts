fe2af685d6e8a341d139d480f2a99ec39adeac79
sergioeabarcaf <sergioaf1991@gmail.com>
2018-03-10 23:04:39 -0300

    se enlaza la propiedad del usuario al ngModel, donde [] solo lee los valor mientras que [()] los lee y modifica

--- angular/7 - formularios/src/app/components/template/template.component.ts
+++ angular/7 - formularios/src/app/components/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -7,13 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+  usuario:Object = {
+    nombre: "sergio",
+    apellido: "abarca",
+    email: "sergio@perro.com"
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  guardar(){
+  guardar( forma:NgForm ){
     console.log("formulario posteado");
+    console.log("ngForm: ", forma);
+    console.log("valor: ", forma.value );
+    console.log("usuario: ", this.usuario);
   }
 
 }
