98c0ae35ef77e823d368eb19ff0c5f054c06ceb9
sergioeabarcaf <sergioaf1991@gmail.com>
2018-03-21 08:59:54 -0300

    se crea un objeto para manejar con objetos dentro de objetos

--- angular/7 - formularios/src/app/components/data/data.component.ts
+++ angular/7 - formularios/src/app/components/data/data.component.ts
@@ -9,12 +9,23 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class DataComponent {
 
   forma:FormGroup;
+  usuario:Object = {
+    nombreCompleto: {
+      nombre: "sergio",
+      apellido: "abarca"
+    },
+    email: "sergioabarca@hotmail.com"
+  }
 
   constructor() {
 
+    console.log( this.usuario );
+
     this.forma = new FormGroup({
-      'nombre': new FormControl('',Validators.required),
-      'apellido': new FormControl('',[Validators.required, Validators.minLength(3)]),
+      'nombreCompleto': new FormGroup({
+        'nombre': new FormControl('',Validators.required),
+        'apellido': new FormControl('',[Validators.required, Validators.minLength(3)])
+      }),
       'email': new FormControl('',[ Validators.required , Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
     })
 
@@ -23,6 +34,7 @@ export class DataComponent {
   guardarCambios(){
     console.log( this.forma.value );
     console.log( this.forma );
+
   }
 
 
