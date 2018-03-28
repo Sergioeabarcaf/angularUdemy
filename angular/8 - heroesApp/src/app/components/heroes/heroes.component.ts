509d1ee51dc1c86f18a775bc0b72dab6b3a6c653
sergioeabarcaf <sergioaf1991@gmail.com>
2018-03-28 15:06:28 -0300

    se agregan alertas
    Se agregan alertas para cuando no hay registros y cuando esta cargando

--- angular/8 - heroesApp/src/app/components/heroes/heroes.component.ts
+++ angular/8 - heroesApp/src/app/components/heroes/heroes.component.ts
@@ -10,10 +10,12 @@ import { Heroe } from '../../intefaces/heroe.interface';
 export class HeroesComponent implements OnInit {
 
   heroes:Heroe[] = [];
+  loading:boolean = true;
 
   constructor(private _heroeService:HeroesService) {
     this._heroeService.getHeroes().subscribe( data => {
       this.heroes = data;
+      this.loading = false;
     });
   }
 
@@ -21,11 +23,13 @@ export class HeroesComponent implements OnInit {
   }
 
   eliminarHeroe(key:string){
+    this.loading = true;
     this._heroeService.deleteHeroe(key).subscribe( data => {
       if(data){
         console.error(data);
       }
       else{
+        this.loading = false;
         delete this.heroes[key];
       }
     });
