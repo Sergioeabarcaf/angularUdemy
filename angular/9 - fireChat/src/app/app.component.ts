4ea73d2e8bcae7fec94c6b3b9366b9bac68ce157
sergioeabarcaf <sergioaf1991@gmail.com>
2018-04-09 10:08:53 -0300

    implementacion logout y auth twitter
    Implementacion de logout y login via twitter

--- angular/9 - fireChat/src/app/app.component.ts
+++ angular/9 - fireChat/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
-import { AngularFirestore } from 'angularfire2/firestore';
-import { Observable } from 'rxjs/Observable';
+import { ChatService } from './services/chat.service';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +8,10 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public chats: Observable<any[]>;
 
-  constructor(db: AngularFirestore) {
-    this.chats = db.collection('chats').valueChanges();
+  constructor( public _chatService:ChatService ) {
+
   }
+
+  
 }
