bd2fba4d4492e0c60df74f2804ba5844c2607654
sergioeabarcaf <sergioaf1991@gmail.com>
2018-04-09 09:44:25 -0300

    creacion de componente login
    Se authoriza en firebase el login con google y se implementan los
    metodos del login.

--- angular/9 - fireChat/src/app/app.module.ts
+++ angular/9 - fireChat/src/app/app.module.ts
@@ -12,12 +12,14 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
 import { ChatComponent } from './components/chat/chat.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent
+    ChatComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- angular/9 - fireChat/src/app/services/chat.service.ts
+++ angular/9 - fireChat/src/app/services/chat.service.ts
@@ -1,14 +1,37 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Mensaje } from '../interfaces/mensaje.interface';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Injectable()
 export class ChatService {
 
   private itemsCollection: AngularFirestoreCollection<Mensaje>;
   public chats:Mensaje[] = [];
+  public usuario:any = {};
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(private afs: AngularFirestore, public afAuth: AngularFireAuth) {
+    this.afAuth.authState.subscribe(user =>{
+      console.log('estado del usuario:', user);
+      if( !user ){
+        return;
+      }
+      else{
+        this.usuario.nombre = user.displayName;
+        this.usuario.uid = user.uid;
+      }
+
+    })
+  }
+
+  login( proveedor:string ) {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 
   cargarMensajes(){
      this.itemsCollection = this.afs.collection<Mensaje>('chats', ref => ref.orderBy("fecha","desc").limit(5));
