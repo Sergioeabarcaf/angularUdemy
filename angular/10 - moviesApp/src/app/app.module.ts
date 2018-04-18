bde63185d7c90e8b4400b3038d1ae8dbba3d78e9
sergioeabarcaf <sergioaf1991@gmail.com>
2018-04-18 00:35:09 -0300

    formulario busqueda
    Se usa formGroup para realizar la busqueda y se usa la funcion buscar
    sel servicio y funciona de manera correcta

--- angular/10 - moviesApp/src/app/app.module.ts
+++ angular/10 - moviesApp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { TmdbService } from './services/tmdb.service';
 
@@ -20,13 +20,14 @@ import { MovieComponent } from './components/movie/movie.component';
     HomeComponent,
     NavbarComponent,
     SearchComponent,
-    MovieComponent
+    MovieComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientJsonpModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     APP_ROUTING
   ],
   providers: [TmdbService],
--- angular/10 - moviesApp/src/app/components/search/search.component.ts
+++ angular/10 - moviesApp/src/app/components/search/search.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { TmdbService } from '../../services/tmdb.service';
+import { FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent{
 
-  constructor() { }
+  form:FormGroup;
 
-  ngOnInit() {
+  arrMovies:any;
+
+  constructor(private _tmdbService:TmdbService) {
+    this.form = new FormGroup({
+      'movies' : new FormControl('',[Validators.required, Validators.minLength(2)])
+    })
+  }
+
+  buscar(){
+    this._tmdbService.buscarPelicula(this.form.controls.movies.value).subscribe( data => this.arrMovies = data);
+    console.log(this.arrMovies);
   }
 
 }
