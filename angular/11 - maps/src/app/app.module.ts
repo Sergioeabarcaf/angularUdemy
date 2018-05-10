33edce993ec3150ffb4edb144931510e9cc6a0c3
sergioeabarcaf <sergioaf1991@gmail.com>
2018-05-10 08:20:23 -0300

    instalacion angular-mapa
    Se agrega angular-map a el proyecto y se obtiene la api-key de google.
    se inserta en app.component.html

--- angular/11 - maps/src/app/app.module.ts
+++ angular/11 - maps/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
+import { AgmCoreModule } from '@agm/core';
+
+import { AppComponent } from './app.component';
+import { MapaComponent } from './components/mapa/mapa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAvbzk1uMTl_c212foc7n6wr4C5CnL2XLo'
+    }),
     MaterialModule
   ],
   providers: [],
