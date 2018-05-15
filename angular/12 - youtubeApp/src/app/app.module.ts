38be753a64c126a6a1619f1f6b6bb2f337532425
sergioeabarcaf <sergioaf1991@gmail.com>
2018-05-15 08:03:12 -0400

    creacion componentes
    Se crean los componentes home y navbar (este ya esta listo para su uso)
    y el servicio de youtube

--- angular/12 - youtubeApp/src/app/app.module.ts
+++ angular/12 - youtubeApp/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { APP_ROUTING } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+import { YoutubeService } from './services/youtube.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [YoutubeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
