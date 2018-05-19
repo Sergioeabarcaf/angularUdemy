8fbf9352b07127f0b87ccb516cf92cd76c3273dc
sergioeabarcaf <sergioaf1991@gmail.com>
2018-05-18 22:32:32 -0400

    validaciones
    Se crean validaciones para saber que no esta cargado el archivo, si es
    una imagen y que no se abra la imagen al colocarlo en el navegador

--- angular/13 - photoFirebase/src/app/directives/ng-drop-files.directive.ts
+++ angular/13 - photoFirebase/src/app/directives/ng-drop-files.directive.ts
@@ -1,10 +1,13 @@
 import { Directive, EventEmitter, ElementRef, HostListener, Input, Output } from '@angular/core';
+import { FileItem } from '../models/file-item';
 
 
 @Directive({
   selector: '[appNgDropFiles]'
 })
 export class NgDropFilesDirective {
+
+  @Input() archivos:FileItem[] = [];
 
   @Output() mouseSobre: EventEmitter<boolean> = new EventEmitter();
 
@@ -19,4 +22,35 @@ export class NgDropFilesDirective {
   public ondragleave( event:any ){
     this.mouseSobre.emit(false);
   }
+
+  //Validaciones
+
+  private archivoPuedeCargarlo( archivo: File ): boolean{
+    if(!this._archivoDropeado(archivo.name) && this._tipoArchivo( archivo.type)){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  private _prevenirDetener( event ){
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  private _archivoDropeado( nombreArchivo:string):boolean{
+    for(const archivo of this.archivos){
+      if( archivo.nombreArchivo == nombreArchivo){
+        console.log('El nombre ya existe');
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private _tipoArchivo( tipoArchivo:string ):boolean {
+    return (tipoArchivo === '' || tipoArchivo === undefined )? false : tipoArchivo.startsWith('image');
+  }
+
 }
