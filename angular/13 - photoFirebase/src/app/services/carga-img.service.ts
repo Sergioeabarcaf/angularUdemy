5556900225538fb78c9567446370fd0a70cc19c0
sergioeabarcaf <sergioaf1991@gmail.com>
2018-05-19 10:12:20 -0400

    carga imagenes a firestore
    Se carga la imagen y luego se guarda una ruta en DB para obtener la
    imagen.
    
    Una vez completado el progreso, se coloca el texto verde.

--- angular/13 - photoFirebase/src/app/services/carga-img.service.ts
+++ angular/13 - photoFirebase/src/app/services/carga-img.service.ts
@@ -10,9 +10,30 @@ export class CargaImgService {
 
   constructor(private db: AngularFirestore) { }
 
+  //Subir a storage
+
   cargarImgFirebase( imagenes:FileItem[] ){
-    console.log(imagenes);
+    const storageRef = firebase.storage().ref();
+    for( const item of imagenes ){
+      item.estaSubiendo = true;
+      if( item.progreso >= 100 ){
+        continue;
+      }
+      const uploadTask: firebase.storage.UploadTask = storageRef.child(`${ this.CARPETA_IMG }/${ item.nombreArchivo }`).put( item.archivo );
+      uploadTask.on( firebase.storage.TaskEvent.STATE_CHANGED,
+        ( snapshot:firebase.storage.UploadTaskSnapshot ) => item.progreso = ( snapshot.bytesTransferred / snapshot.totalBytes ) * 100,
+        ( error ) => console.error(error),
+        () => {
+          console.log('Imagen cargada');
+          item.url = uploadTask.snapshot.downloadURL;
+          item.estaSubiendo = false;
+          this.guardarImg({ nombre: item.nombreArchivo, url: item.url });
+        }
+      )
+    }
   }
+
+  //Gurdar referencia en DB
 
   private guardarImg( imagen:any ){
     this.db.collection(`/${ this.CARPETA_IMG }`).add( imagen );
