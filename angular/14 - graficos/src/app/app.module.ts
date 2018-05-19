429e1efbbf5662000ed5e7f4725cb514494243d0
sergioeabarcaf <sergioaf1991@gmail.com>
2018-05-19 12:07:54 -0400

    uso de grafico de lineas

--- angular/14 - graficos/src/app/app.module.ts
+++ angular/14 - graficos/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
+import { LineComponent } from './components/line/line.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineComponent
   ],
   imports: [
     BrowserModule,
